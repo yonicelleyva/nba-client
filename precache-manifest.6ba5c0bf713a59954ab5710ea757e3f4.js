78ae452d2025e4eb924579a94ccb4a8dc00bb2fd
Yonicel Leyva <yonicelleyva@yonicels-mbp.lan>
2019-01-23 19:15:17 -0500

    Updates

---
+++ precache-manifest.6ba5c0bf713a59954ab5710ea757e3f4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "63e4249d972eaf67aa51",
+    "url": "/nba-client/static/css/main.35606451.chunk.css"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "32ec6570d969d156aa25",
+    "url": "/nba-client/static/js/1.32ec6570.chunk.js"
+  },
+  {
+    "revision": "74a0c7e45e43d5b8686a",
+    "url": "/nba-client/static/js/runtime~main.74a0c7e4.js"
+  },
+  {
+    "revision": "63e4249d972eaf67aa51",
+    "url": "/nba-client/static/js/main.63e4249d.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "32ec6570d969d156aa25",
+    "url": "/nba-client/static/css/1.33a99dd6.chunk.css"
+  },
+  {
+    "revision": "25225ef23db1cd09b0b4662ea0fc3dc5",
+    "url": "/nba-client/index.html"
+  }
+];
