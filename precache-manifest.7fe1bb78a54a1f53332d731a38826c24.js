5dced4e8f37ba8551ca4fbb09a9c51b120d3e9df
Yonicel Leyva <yonicelleyva@yonicels-mbp.lan>
2019-01-25 20:14:51 -0500

    Updates

--- precache-manifest.7fe1bb78a54a1f53332d731a38826c24.js
+++ precache-manifest.7fe1bb78a54a1f53332d731a38826c24.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "f4769f9bdb7466be65088239c12046d1",
-    "url": "/nba-client/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
-    "revision": "5633d205d7a8f672cafd",
+    "revision": "228b9425624cc0c9cb27",
     "url": "/nba-client/static/css/main.35606451.chunk.css"
   },
   {
@@ -20,27 +20,27 @@ self.__precacheManifest = [
     "url": "/nba-client/static/js/runtime~main.74a0c7e4.js"
   },
   {
-    "revision": "5633d205d7a8f672cafd",
-    "url": "/nba-client/static/js/main.5633d205.chunk.js"
+    "revision": "228b9425624cc0c9cb27",
+    "url": "/nba-client/static/js/main.228b9425.chunk.js"
   },
   {
-    "revision": "fa2772327f55d8198301fdb8bcfc8158",
-    "url": "/nba-client/static/media/glyphicons-halflings-regular.fa277232.woff"
-  },
-  {
-    "revision": "448c34a56d699c29117adc64c43affeb",
-    "url": "/nba-client/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
     "url": "/nba-client/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
   },
   {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/nba-client/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
     "revision": "32ec6570d969d156aa25",
     "url": "/nba-client/static/css/1.33a99dd6.chunk.css"
   },
   {
-    "revision": "421109aa04b1af5378f015511311356a",
+    "revision": "fce3fd48f3bf30b5797f57dc8e76ca95",
     "url": "/nba-client/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nba-client/precache-manifest.47069870559c89562baa6035bc4e2091.js"
+  "/nba-client/precache-manifest.7fe1bb78a54a1f53332d731a38826c24.js"
 );
 
 workbox.clientsClaim();
